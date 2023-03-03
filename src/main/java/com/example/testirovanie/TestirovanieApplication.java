47b2839baa2ecb53aa0d7543f879bd1ace03d20a
Антон <antonolegovich2017@icloud.com>
2023-03-03 18:18:47 +0600

    DZ

--- src/main/java/com/example/testirovanie/TestirovanieApplication.java
+++ src/main/java/com/example/testirovanie/TestirovanieApplication.java
@@ -8,6 +8,9 @@ public class   TestirovanieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestirovanieApplication.class, args);
+
+
+
 	}
 
 }
---
+++ src/main/java/com/example/testirovanie/User.java
@@ -0,0 +1,28 @@
+package com.example.testirovanie;
+
+public class User {
+    private static String login;
+    public static String email;
+
+    public User(String login, String email) {
+        if (login.length() != email.length() || login!=null|| !login.isEmpty()||!login.isBlank())
+            this.login = login;
+
+        if (email.contains("@") || email.contains(".")||email!=null||!email.isEmpty()||!email.isBlank()) {
+            this.email = email;
+        }
+    }
+
+
+    public User() {
+    }
+
+    public static String getLogin() {
+        return login;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+}
